25874ed0f961bd5689e20a8615da6aa5da87712b
Jack <Jack@DESKTOP-5B48EHR>
2017-10-03 21:32:30 +0800

    add Way.WindowsService

---
+++ Way.WindowsService/Way.WindowsService/Config.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Way.WindowsService
+{
+    class Config
+    {
+        public static string ServiceName;
+        public static string Application;
+        public static string Args;
+        static Config()
+        {
+            string appPath = System.IO.Path.GetDirectoryName( typeof(Config).Assembly.CodeBase.Replace("file:///", "")) + "\\";
+            try
+            {
+               // System.IO.File.WriteAllText(@"J:\testservice\noerr.txt", appPath );
+                XmlDocument xmldoc = new XmlDocument();
+                xmldoc.Load(appPath + "config.xml");
+                ServiceName = xmldoc.DocumentElement.Attributes["ServiceName"].InnerText.Replace(" " , "");
+                Application = xmldoc.DocumentElement.Attributes["Application"].InnerText;
+                Args = xmldoc.DocumentElement.Attributes["Args"].InnerText.Replace("{0}" , AppDomain.CurrentDomain.BaseDirectory);
+                
+            }
+            catch(Exception ex)
+            {
+               // System.IO.File.WriteAllText(@"J:\testservice\err.txt", appPath + "\r\n" + ex.ToString());
+            }
+        }
+    }
+}
